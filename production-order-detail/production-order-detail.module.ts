eee84fa593655b6e0641e8fd3f54d3430d029bab
thienvo <111thienvo@gmail.com>
2024-06-07 19:19:56 +0700

    create production order

---
+++ production-order-detail/production-order-detail.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { ShareModule } from 'src/app/share.module';
+import { ProductionOrderDetailPage } from './production-order-detail.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductionOrderDetailPage,
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule, ShareModule, RouterModule.forChild(routes)],
+  declarations: [ProductionOrderDetailPage],
+})
+export class ProductionOrderDetailPageModule {}
---
+++ production-order/production-order.page.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController, AlertController, LoadingController, PopoverController } from '@ionic/angular';
+import { EnvService } from 'src/app/services/core/env.service';
+import { PageBase } from 'src/app/page-base';
+import { BRA_BranchProvider, PROD_BillOfMaterialsProvider } from 'src/app/services/static/services.service';
+import { Location } from '@angular/common';
+import { lib } from 'src/app/services/static/global-functions';
+
+@Component({
+  selector: 'app-production-order',
+  templateUrl: 'production-order.page.html',
+  styleUrls: ['production-order.page.scss'],
+})
+export class ProductionOrderPage extends PageBase {
+  typeList = [];
+
+  constructor(
+    public pageProvider: PROD_BillOfMaterialsProvider,
+    public branchProvider: BRA_BranchProvider,
+    public modalController: ModalController,
+    public popoverCtrl: PopoverController,
+    public alertCtrl: AlertController,
+    public loadingController: LoadingController,
+    public env: EnvService,
+    public navCtrl: NavController,
+    public location: Location,
+  ) {
+    super();
+  }
+
+  preLoadData(event) {
+    this.sort.Id = 'Id';
+    this.sortToggle('Id', true);
+    super.preLoadData(event);
+  }
+
+  loadedData(event) {
+    this.env.getType('BOMType').then((data) => {
+      this.typeList = data;
+      this.items.forEach((i) => {
+        i.TypeName = lib.getAttrib(i.Type, this.typeList, 'Name', '', 'Code');
+      });
+      super.loadedData(event);
+    });
+  }
+
+  exportClick() {
+    if (this.submitAttempt) return;
+    this.submitAttempt = true;
+    this.env
+      .showLoading('Vui lòng chờ export dữ liệu...', this.pageProvider.export(this.query))
+      .then((response: any) => {
+        this.downloadURLContent(response);
+        this.submitAttempt = false;
+      })
+      .catch((err) => {
+        this.submitAttempt = false;
+      });
+  }
+
+}
